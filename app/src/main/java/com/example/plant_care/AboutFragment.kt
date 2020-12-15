b4d263268f589588d961e7f6bfeb41b9883cf0aa
arcanine31 <madmaw215@gmail.com>
2020-12-15 16:54:42 +0700

    adding notif channel and create calling notification

--- app/src/main/java/com/example/plant_care/AboutFragment.kt
+++ app/src/main/java/com/example/plant_care/AboutFragment.kt
@@ -1,18 +1,30 @@
 package com.example.plant_care
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_about.*
 
 class AboutFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        initListener()
+    }
+
+    fun initListener(){
+        bt_notif.setOnClickListener(){
+            (activity as MainActivity?)?.sendNotification()
+        }
     }
 
 
--- app/src/main/java/com/example/plant_care/MainActivity.kt
+++ app/src/main/java/com/example/plant_care/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.plant_care
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_add.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val CHANNEL_ID = "channel_example"
+    private val notificationID = 101
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
 
         bottom_navigation.setOnNavigationItemSelectedListener(onBottomNavListener)
 
@@ -42,5 +55,33 @@ class MainActivity : AppCompatActivity() {
         fr.commit()
 
         true
+    }
+
+
+    fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "NotificationTitileExample"
+            val textDesc = "NotificationDescExample"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = textDesc
+            }
+
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+        }
+    }
+
+    fun sendNotification(){
+        val notifBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.logoplant)
+                .setContentTitle("NotifContentTitle")
+                .setContentText("NotifContentText")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationID, notifBuilder.build())
+        }
     }
 }
